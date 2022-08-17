c3c25f6f3642ecf8c26194d2f790ee315de0c557
alfamidia-git <marlon@alfamidia.com.br>
2022-08-17 20:07:19 -0300

    Criei uma nova regra para ver se o nr e maior que permitido

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import exception.NumeroMaiorQueOPermitidoException;
 import exception.NumeroMenorQueOPermitidoException;
 
 public class Principal {
@@ -12,18 +13,23 @@ public class Principal {
 		boolean continua = true;
 		do {
 			try {
-				System.out.println("Digite um número um número maior que 5: ");
+				System.out.println("Digite um número um número maior que 5 e menor que 50: ");
 				x = sc.nextInt();
 				continua = false;
 				
 				if(x < 5) {
 					throw new NumeroMenorQueOPermitidoException("Número menor ou igual a 5...");
+				}else if(x > 50) {
+					throw new NumeroMaiorQueOPermitidoException("Número maior ou igual a 50...");
 				}
 			} catch (InputMismatchException erro) {
 				System.out.println("Opss, tipo não permitido");
 				erro.printStackTrace();
 				sc.nextLine();
 			}catch(NumeroMenorQueOPermitidoException erro) {
+				erro.printStackTrace();
+			}
+			catch(NumeroMaiorQueOPermitidoException erro) {
 				erro.printStackTrace();
 			}
 			
